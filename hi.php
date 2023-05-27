97a58d6873c31f6643fd58dcf9aa86a7b7825fa4
Anton Yurchenko <satnetuser001@gmail.com>
2023-05-27 19:55:07 +0300

    commit N6 by satnetuser001 for final testing Personal access tokens (classic)

--- hi.php
+++ hi.php
@@ -1,5 +1,5 @@
 <?php
 //file in upstream repository made satnetuser003 for testing pull request
-//commit N5 by satnetuser001 for final testing ssh key
+//commit N6 by satnetuser001 for final testing Personal access tokens (classic)
 echo "hello beautiful world\n";
 ?>
